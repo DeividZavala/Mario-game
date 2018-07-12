e7ba1de98a5ebf89b2e59f96463bdbc00bf89485
DeividZavala <crusedmoss@gmail.com>
2018-07-12 10:25:55 -0500

    final

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var enemies = [];
-var interval;
 //probamos que todo funcione:
 //ctx.fillRect(0,0,50,50);
 
-// var imagen = new Image();
-// imagen.src = 'https://vignette.wikia.nocookie.net/moj-wlasny-swiat/images/7/75/Mario.png/revision/latest?cb=20141208165919&path-prefix=pl';
-// imagen.onload = function(){
-//     ctx.drawImage(imagen, 0,0,50,100);
-// }
-
-
-// Clases
-
 class Mario{
-    constructor(){
+    constructor(apellido){
+        this.apellido = apellido
+        this.imagen1 = new Image()
+        this.imagen1.src = 'https://bit.ly/2L7yH3f'
+        this.imagen2 = new Image()
+        this.imagen2.src = 'https://bit.ly/2L3ikoe'
+        this.imagen = this.imagen1;
         this.x = 10;
         this.y = 295;
-        this.width = 20;
+        this.width = 30;
         this.height = 40;
-        this.image1 = new Image();
-        this.image1.src = "https://bit.ly/2L7yH3f";
-        this.image2 = new Image();
-        this.image2.src = 'https://bit.ly/2L3ikoe';
-        this.image = this.image1;
     }
 
 
@@ -35,52 +25,52 @@ class Mario{
             (this.y + this.height > item.y);
     }
 
+    
     draw(){
-
         if(this.y < 295) this.y += 4;
-
         if(frames % 10 === 0){
-            this.image = this.image == this.image1 ? this.image2 : this.image1;
+             this.imagen = this.imagen == this.imagen1 ? this.imagen2 : this.imagen1;
         }
-        ctx.drawImage(this.image, this.x, this.y, 30,40);
+        ctx.drawImage(this.imagen, this.x, this.y, this.width,this.height);
     }
-
 }
 
 class Background{
     constructor(){
-        this.x = 0;
-        this.y = 0;
-        this.width = canvas.width;
-        this.height = canvas.height;
-        this.image = new Image();
-        this.image.src = 'https://bit.ly/2m9qY9Q';
+        this.x = 0
+        this.y = 0
+        this.width = canvas.width
+        this.height = canvas.height
+        this.imagen = new Image()
+        this.imagen.src = 'https://bit.ly/2m9qY9Q'
     }
-
 
     gameOver(){
+        // Detenemos la ejecución del intervalo
         clearInterval(interval);
+        // Definimos el tamaño y fuente de nuestro texto
         ctx.font = "80px Avenir";
+        // Dibujamos el texto en el canvas.
         ctx.fillText("Game Over", 250, 190);
-        ctx.fillStyle = "red";
     }
-
-
+    
     draw(){
-        this.x --;
-        if(this.x < -canvas.width) this.x = 0;
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+       // restamos en x para moverlo
+       this.x--;
+       // en caso de alcanzar el final de la imagen reseteamos x
+       if(this.x < -canvas.width) this.x = 0;
+       ctx.drawImage(this.imagen,this.x,this.y,this.width,this.height); 
+     // dibujamos una segunda imagen al final de la primera
+      ctx.drawImage(this.imagen,this.x + this.width,this.y,this.width,this.height); 
     }
-
 }
-
 
 
 class Enemy{
     constructor(){
-        //de principio el enemigo aparece fuera del canvas
+    //de principio el enemigo aparece fuera del canvas
         this.x = canvas.width;
+        //el y del enemigo es el mismo de mario
         this.y = 295;
         this.width = 20;
         this.height = 40;
@@ -89,27 +79,27 @@ class Enemy{
     }
 
     draw(){
+        //el y del enemigo es el mismo de mario
         if(frames % 10) this.x -= 5;
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
 }
 
 
-// Instancias
 
-var mario = new Mario();
-var background = new Background;
-var enemy = new Enemy;
+var fondo = new Background();
+var marioPerez = new Mario("Perez");
 
-
-// setinterval
 var frames = 0;
-
 var interval = setInterval(function(){
+    // sumamos cada cuadro que dibujamos
     frames++
+    // borramos el canvas
     ctx.clearRect(0,0,256,256);
-    background.draw();
-    mario.draw();
+    // Dibujamos a mario y el background
+    fondo.draw();
+    marioPerez.draw();
+    // Generamos enemigos
     generateEnemies();
     drawingEnemies();
 }, 1000/60)
@@ -117,15 +107,16 @@ var interval = setInterval(function(){
 
 addEventListener('keydown', function(event){
     if(event.keyCode === 32){
-        mario.y -= 80;
+        marioPerez.y -= 80;
     }
 })
 
 
-// funciones auxiliares
+var enemies = [];
 
 function generateEnemies() {
     if(frames % 100 == 0 || frames % 60 == 0 || frames % 170 == 0){
+        // creamos una instancia de Enemy y la agregamos aun arreglo
         var enemy = new Enemy
         enemies.push(enemy);
     }
@@ -134,8 +125,9 @@ function generateEnemies() {
 function drawingEnemies(){
     enemies.forEach(function(enemy){
         enemy.draw()
-        if(mario.collision(enemy)){
-            background.gameOver();
+        if(marioPerez.collision(enemy)){
+            //Ejecutamos el gameOver
+            fondo.gameOver();
         }
     })
 }
